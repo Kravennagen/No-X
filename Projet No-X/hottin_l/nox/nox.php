b774598c7ca546675026c72fb7b181185f7efe48
hottin_l <hottin_l@d120ecaa-a4d6-4b97-b5ee-0fc023841a04>
2014-11-07 12:58:20 +0000

    Projet fini reste les options

--- Projet No-X/hottin_l/nox/nox.php
+++ Projet No-X/hottin_l/nox/nox.php
@@ -6,7 +6,7 @@
 // Login   <hottin_l@etna-alternance.net>
 // 
 // Started on  Thu Nov  6 09:17:07 2014 HOTTIN Lucie
-// Last update Thu Nov  6 17:53:55 2014 HOTTIN Lucie
+// Last update Fri Nov  7 13:53:06 2014 HOTTIN Lucie
 //
 
 require_once "./include/error_management.php";
@@ -14,8 +14,8 @@ require_once "./include/error_management.php";
 // PENSER A SUPPRIMER TOUS LES COMMENTAIRES DANS TOUS LES FICHIERS !!!!!!
 
 system("clear");
-$j = 1;                      // le nombre d'arguments
-$timestart = microtime(TRUE);
+$j = 1;
+$deb = microtime(TRUE);
 $recherche = 1;
 if ($argc< 3)
   echo "Vous devez saisir au moins 3 arguments : un fichier exécutable en premier, un dico et un message\n";
@@ -25,33 +25,37 @@ else
       {
 	$param = $argv[$j];
 	if ($param == "dico")
-	    $j++;
+	  $j++;
 	elseif (error_management($param) == TRUE)
 	  {
 	    echo "\033[01;32m\nBienvenue Agent N˚X\n\n\033[00m";	
-	    $file = fopen($param, "r");
-	    $content = file_get_contents($param);    // Va servir pour rechercher chaque mot du fichier dans le dictionnaire wesh
+	    $content = file_get_contents($param);
 	    echo "\033[04mRecherche n˚$recherche :\033[00m\n\n";
-	    //	    print_r(explode(' ', $content));
+	    $message = explode(" ", trim($content));
+	    $content2 = file_get_contents("dico");
+	    $dico = explode(" ", trim($content2));
 	    $i = 0;
-	    $tab = $content;
-	    while ($i < 4)
+	    while (isset($message[$i]))
 	      {
-		$reg1 = "/[0.9][06][16][9.3]/";
-		//		if ($content[$i] == $reg1)
-		print_r(explode(' ', $content));
-		print_r($tab);
-		echo "\n";
+		$k = 0;		
+		while (isset($dico[$k]))
+		  {
+		    if ($message[$i] != $dico[$k])
+		      $k++;
+		    else
+		      {
+			echo "$message[$i]\n";
+			break;
+		      }
+		  }		    
 		$i++;
 	      }
-	    //	    readfile($param);                        // A modifier pour qu'il affiche le mot souhaité
-	    fclose($file);
 	    $j++;
 	    $recherche++;
 	  }
       }
-    $timeend = microtime(TRUE);
-    $time = $timeend - $timestart;
-    $page_load_time = number_format($time, 4);
-    echo "\nRecherche terminée en " . $page_load_time . " sec.\n\n";
+    $fin = microtime(TRUE);
+    $time = $fin - $deb;
+    $form = number_format($time, 5);
+    echo "\nRecherche terminée en " . $form . " sec.\n\n";
   }
